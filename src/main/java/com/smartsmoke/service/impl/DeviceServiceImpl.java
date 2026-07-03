ecb09f4689cea89615cd0365a901e7b4c1f15c52
tc <844186424@qq.com>
2026-07-03 16:24:13 +0800

    1

--- src/main/java/com/smartsmoke/service/impl/DeviceServiceImpl.java
+++ src/main/java/com/smartsmoke/service/impl/DeviceServiceImpl.java
@@ -1,6 +1,7 @@
 package com.smartsmoke.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.smartsmoke.entity.DeviceStatusStatsVO;
 import com.smartsmoke.entity.SmokeDevice;
 import com.smartsmoke.mapper.DeviceMapper;
 import com.smartsmoke.service.DeviceService;
@@ -8,4 +9,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DeviceServiceImpl extends ServiceImpl<DeviceMapper, SmokeDevice> implements DeviceService {
+
+    @Override
+    public DeviceStatusStatsVO getStats() {
+        return baseMapper.getDeviceStats();
+    }
 }
